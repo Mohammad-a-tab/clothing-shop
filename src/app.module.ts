71ba4148c203a045e13e2abc8edc9048c197a35b
mohammad-a-tab <mohammad.programmer.tab@gmail.com>
2023-05-28 01:30:18 +0330

    make changes

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ProductsController } from './products/products.controller';
+import { ProductsModule } from './products/products.module';
+import { CategoriesService } from './categories/categories.service';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [ProductsModule],
+  controllers: [AppController, ProductsController],
+  providers: [AppService, CategoriesService],
 })
 export class AppModule {}
