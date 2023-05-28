0349f3fe3ef03cf8c6c6cc1bea116e614d5ae655
mohammad-a-tab <mohammad.programmer.tab@gmail.com>
2023-05-29 01:01:26 +0330

    create users section

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,10 +5,12 @@ import { ProductsController } from './products/products.controller';
 import { ProductsModule } from './products/products.module';
 import { CategoriesService } from './categories/categories.service';
 import { CategoriesModule } from './categories/categories.module';
+import { UsersController } from './users/users.controller';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [ProductsModule, CategoriesModule],
-  controllers: [AppController],
+  imports: [ProductsModule, CategoriesModule, UsersModule],
+  controllers: [AppController, UsersController],
   providers: [AppService],
 })
 export class AppModule {}
