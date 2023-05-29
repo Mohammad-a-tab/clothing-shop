dc20d3a1f4040f65d8b8eb16b22e0b83d947e12c
mohammad-a-tab <mohammad.programmer.tab@gmail.com>
2023-05-29 23:56:29 +0330

    make changes

--- src/products/products.entity.ts
+++ src/products/products.entity.ts
@@ -1,14 +1,16 @@
-import { Column, CreateDateColumn, Entity } from "typeorm";
+import { Column, Entity } from "typeorm";
 @Entity('products')
 export class Product {
     @Column({ nullable: true })
     title: string;
-    @Column({ nullable: false, unique: true })
+    @Column({ nullable: false })
     description: string;
     @Column({ nullable: false })
-    size: string
+    size: string;
     @Column({ nullable: false })
-    price: string
-    @Column({ default: "" })
-    images: [string];
+    price: string;
+    @Column({ nullable: false })
+    color: string;
+    @Column({ type: 'varchar', array: true })
+    images: string;
 }
