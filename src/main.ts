f62677925196dc4c76b9f1bebe6d6277f96dfe8c
mohammad-a-tab <mohammad.programmer.tab@gmail.com>
2023-05-31 00:15:30 +0330

    start and implement swagger for this project

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const options = new DocumentBuilder()
+  .setTitle('Clothing Project')
+  .setDescription('Build, implement and test clothing project APIs')
+  .setVersion('3.0')
+  .addBearerAuth()
+  .build();
+
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api-docs', app, document);
+  
   await app.listen(3000);
 }
 bootstrap();
