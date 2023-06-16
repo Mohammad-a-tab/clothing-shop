270fbf9cb875d9e29c2bc3c45e02d2b3458c949d
mohammad-a-tab <mohammad.programmer.tab@gmail.com>
2023-06-16 19:58:26 +0330

    make changes

--- src/utils/functions.ts
+++
@@ -1,9 +0,0 @@
-export function stringToArray (fields: string): Array<string> {
-    let array: string[] = [];
-    if(fields.indexOf(",") >=0){
-        array = fields.split(",").map(item => item.trim())
-    }else {
-        array = []
-    }
-    return array;
-}
