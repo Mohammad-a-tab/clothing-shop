b8d0b0a83f6b5a2719240d02871e1cbf9bceecb7
mohammad-a-tab <mohammad.programmer.tab@gmail.com>
2023-06-16 19:55:21 +0330

    delete midlewares

--- src/products/product.module.ts
+++ src/products/product.module.ts
@@ -4,15 +4,10 @@ import { ProductController } from './product.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Product } from './product.entity';
 import { ProductRepository } from './product.repository';
-import { StringToArray } from 'src/middlewares/stringToArray';
 
 @Module({
   imports : [TypeOrmModule.forFeature([Product])],
   controllers : [ProductController],
   providers : [ProductService, ProductRepository],
 })
-export class ProductModule {
-  configure(consumer: MiddlewareConsumer) {
-    consumer.apply(StringToArray).forRoutes('products', 'add');
-  }
-}
+export class ProductModule {}
